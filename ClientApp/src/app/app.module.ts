7e6393661c4e2d001b21953ffdad8fc03efb2775
cristian.urzica <cristian.urzica@softvision.ro>
2020-06-13 23:02:27 +0300

    add new functionality

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { TasksComponent } from './components/tasks/tasks.component';
-import { TaskDetailComponent } from './components/task-detail/task-detail.component';
-import { TaskCreateComponent } from './components/task-create/task-create.component';
-import { TaskEditComponent } from './components/task-edit/task-edit.component';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import {AppComponent} from './app.component';
+import {NavMenuComponent} from './components/nav-menu/nav-menu.component';
+import {HomeComponent} from './components/home/home.component';
+import {TasksComponent} from './components/tasks/tasks.component';
+import {TaskDetailComponent} from './components/task-detail/task-detail.component';
+import {TaskCreateComponent} from './components/task-create/task-create.component';
+import {TaskEditComponent} from './components/task-edit/task-edit.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AngularMaterialModule} from './shared/angular-material.module';
 
 @NgModule({
   declarations: [
@@ -32,9 +34,13 @@ import { TaskEditComponent } from './components/task-edit/task-edit.component';
       {path: 'task-create', component: TaskCreateComponent},
       {path: 'task-edit/:taskId', component: TaskEditComponent},
     ]),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
+  exports: [AngularMaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
